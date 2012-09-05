93d18d206c5ff0960c62d03cd1e2d3333a45e2fc
Greg Moreno <rubyoncloud@gmail.com>
2012-09-04 17:00:56 -0700

    Count status by hour of day

--- counter.rb
+++ counter.rb
@@ -6,5 +6,6 @@ require File.dirname(__FILE__) + "/tweetminer"
 settings = YAML.load_file File.dirname(__FILE__) + '/mongo.yml'
 miner = TweetMiner.new(settings)
 
-results = miner.status_count_by_user
+#results = miner.status_count_by_user
+results = miner.status_count_by_hday
 ap results
--- status_counter.rb
+++ status_counter.rb
@@ -30,6 +30,38 @@ module StatusCounter
     statuses.map_reduce(counter.map_command, counter.reduce_command, default_mr_options)
   end
 
+
+  class HourOfDayCounter
+    def map_command
+      'function() {
+        var re = /(\d{2,2}):\d{2,2}:\d{2,2}/;
+        var hour = re.exec(this.created_at)[1];
+
+        emit(hour, 1);
+      }'
+    end
+
+    def reduce_command
+      <<-EOS
+        function(key, values) {
+          var count = 0;
+
+          for(i in values) {
+            count += values[i]
+          }
+
+          return count;
+        }
+      EOS
+    end
+
+  end
+
+  def status_count_by_hday
+    counter = HourOfDayCounter.new
+    statuses.map_reduce(counter.map_command, counter.reduce_command, default_mr_options)
+  end
+
   def default_mr_options
     {:out => {:inline => 1}, :raw => true }
   end
