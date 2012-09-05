b859585142e893db73e85b5acd0b5b892a7290a6
Greg Moreno <rubyoncloud@gmail.com>
2012-09-04 17:11:53 -0700

    Add days ago filter in map-reduce

--- status_counter.rb
+++ status_counter.rb
@@ -57,9 +57,15 @@ module StatusCounter
 
   end
 
-  def status_count_by_hday
+  def status_count_by_hday(days_ago = 7)
+    date     = Date.today - days_ago
+    days_ago = Time.utc(date.year, date.month, date.day)
+    query = { "created_at_dt" => { "$gte" => days_ago } }
+
+    options = default_mr_options.merge(:query => query)
+
     counter = HourOfDayCounter.new
-    statuses.map_reduce(counter.map_command, counter.reduce_command, default_mr_options)
+    statuses.map_reduce(counter.map_command, counter.reduce_command, options)
   end
 
   def default_mr_options
